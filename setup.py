7baf78c1b31f3d9730d4b31a2455d1e8ed76da11
Ambarish_224 <ambarish.singh224@gmal.com>
2023-04-19 23:17:31 +0530

    Sign Language All File Updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Ambarish Singh',
+    author_email= 'ambarish.singh224@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
